4bdcbce9eb78483a3846afbe3c3a32e03cd972bd
ibrahimnabi20 <ibrahimnabi1357@gmail.com>
2025-05-28 09:03:11 +0200

    almost done

--- backend/obj/Debug/net8.0/StudyTracker.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/StudyTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3d2a526276ef4301a5340e9ab785f36604f66a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20748f8f9920e0d6e4d2f10b18020688570b5966")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
